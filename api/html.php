5bc4d43a8085f5cf8dd2702eb89129fc25d930c8
magicluweibo <86891831+magicluweibo@users.noreply.github.com>
2023-11-06 23:35:51 +0800

    Add files via upload

--- api/html.php
+++ api/html.php
@@ -9,11 +9,15 @@ echo $html_code;
 	
 function zxCurl($url)
 {
+	$headers = [
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+	'Referer: histar.tv'
+    ];
     
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_TIMEOUT, 100);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
